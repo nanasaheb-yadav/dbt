a84266bd86e93d17c97d78cc3b8007d665876931
Nanasaheb Yadav <nanayadav33@gmail.com>
2022-10-31 07:16:42 +0000

    adding advance macro code

--- macros/limit_data_by_days.sql
+++ macros/limit_data_by_days.sql
@@ -1,5 +1,5 @@
 {% macro limit_data_by_days(colname, no_of_days=3) -%}
-
+    {% if target.name == 'dev'%}
     WHERE {{colname}} >= dateadd('day', -{{no_of_days}}, current_timestamp)
-    
+    {% endif %}
 {%- endmacro %}
